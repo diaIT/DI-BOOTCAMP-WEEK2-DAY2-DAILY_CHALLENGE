e3b01eff44c10def7d90c38f316d20545331ce62
Diallo boubacar <diallob456@gmail.com>
2022-12-29 15:07:22 +0100

    DAILY CJALLENGE

---
+++ script.js
@@ -0,0 +1,15 @@
+ 
+ 
+ //*------------------------------------------------Daily Challenge: Not Bad--------------------------------------------------*//
+ 
+ 
+                    const sentence = "The movie is not that bad, I like it"
+                    const wordNot  = sentence.indexOf("not");
+                    const wordBat = sentence.indexOf("bad");
+
+                    if (wordNot < wordBat){
+                         console.log("The movie is good, I like it")
+
+                    } else {
+                          console.log("The movie is not that bad, I like it")
+                           }
